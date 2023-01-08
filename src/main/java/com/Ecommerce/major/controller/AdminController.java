79e450bf00c7cbe48a4cd9c40e9de305c77709cc
sai teja <104936242+saitejaseedella@users.noreply.github.com>
2023-01-09 01:39:25 +0530

    Updated logo, added products category.

--- src/main/java/com/Ecommerce/major/controller/AdminController.java
+++ src/main/java/com/Ecommerce/major/controller/AdminController.java
@@ -2,25 +2,29 @@ package com.Ecommerce.major.controller;
 
 import com.Ecommerce.major.dto.ProductDTO;
 import com.Ecommerce.major.model.Category;
+import com.Ecommerce.major.model.Product;
 import com.Ecommerce.major.service.CategoryService;
-import com.Ecommerce.major.service.ProcustService;
+import com.Ecommerce.major.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 @Controller
 public class AdminController {
+    public static  String uploadDir =System.getProperty("user.dir")+"/src/main/resources/static/productImages";
     @Autowired
   CategoryService categoryService;
 
     @Autowired
-    ProcustService procustService;
+    ProductService procustService;
 
     @GetMapping("/admin")
     public String adminHome() {
@@ -76,6 +80,33 @@ public class AdminController {
         model.addAttribute("productDTO",new ProductDTO());
         model.addAttribute("categories",categoryService.getAllCategory());
         return "productsAdd";
+    }
+
+    @PostMapping("/admin/products/add")
+    public String productAddPost(@ModelAttribute("productDTO")ProductDTO productDTO,
+                                 @RequestParam("productImage")MultipartFile file,
+                                 @RequestParam("imgName") String imgName) throws IOException {
+
+        Product product = new Product();
+        product.setId(productDTO.getId());
+        product.setName(productDTO.getName());
+        product.setCategory(categoryService.getCategoryById(productDTO.getCategoryId()).get());
+        product.setPrice(productDTO.getPrice());
+        product.setWeight(productDTO.getWeight());
+        product.setDescription(productDTO.getDescription());
+
+        String imageUUID;
+        if(!file.isEmpty()){
+            imageUUID=file.getOriginalFilename();
+            Path fileNameAndPath = Paths.get(uploadDir,imageUUID);
+            Files.write(fileNameAndPath,file.getBytes());
+        }else{
+            imageUUID =imgName;
+        }
+        product.setImageName(imageUUID);
+        procustService.addProduct(product);
+        return "redirect:/admin/products";
+
     }
 
 }
